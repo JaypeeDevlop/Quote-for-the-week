dc20b8a5b39643e2f080d4525bd325135167bf19
Julius S. Oludare <365daysofcodes@gmail.com>
2022-08-06 08:53:16 +0100

    Quote Generator

---
+++ generator.js
@@ -0,0 +1,149 @@
+let quote = [];
+
+
+quote = [
+    {quote:"There is always light. If only we're brave enough to see it. If only we're brave enough to be it.", author: "Amanda Gorman"},
+    {quote:"If you want to lift yourself up, lift up someone else.", author:"Booker T. Washington"},
+    {quote:"I have learned not to allow rejection to move me.", author:"Cicely Tyson"},
+    {quote:"You are braver than you believe, stronger than you seem, and smarter than you think.", author:" A.A. Milne"},
+    {quote:"Attitude is the 'little' thing that makes a big difference.", author:" Winston Churchill"},
+    {quote:"All our dreams can come true — if we have the courage to pursue them.", author:" Walt Disney"},
+    {quote:"Opportunity is missed by most people because it is dressed in overalls and looks like work.", author:" Thomas Edison"},
+    {quote:"Don't sit down and wait for the opportunities to come. Get up and make them.", author:" Madam C.J Walker"},
+    {quote:"The people who are crazy enough to think they can change the world are the ones who do.", author:" Steve Jobs"},
+    {quote:"I never dreamed about success. I worked for it.", author:" Esteé Lauder"},
+    {quote:"Champions keep playing until they get it right.", author:" Billie Jean King"},
+    {quote:"We can't help everyone, but everyone can help someone.", author:" Ronald Reagan"},
+    {quote:"People often say that motivation doesn't last. Well, neither does bathing -- that's why we recommend it daily", author:"Zig Ziglar"},
+    {quote:"Someday is not a day of the week.", author:"Denise Brennan-Nelson"},
+    {quote:"If you cannot do great things, do small things in a great way.", author:"Napoleon Hill"},
+    {quote:"The best leaders are those most interested in surrounding themselves with assistants and associates smarter than they are. They are frank in admitting this and are willing to pay for such talents.", author:"Antos Parrish"},
+    {quote:"The bad news is time flies. The good news is you're the pilot.", author:"Michael Altshuler"},
+    {quote:"Success is not final, failure is not fatal: it is the courage to continue that counts.", author:"Winston Churchill"},
+    {quote:"You are never too old to set another goal or to dream a new dream.", author:"Malala Yousafzai"},
+    {quote:"For me, becoming isn’t about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn't end.", author:"Michelle Obama"},
+    {quote:"You don't always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.", author:"Mandy Hale"},
+    {quote:"What lies behind you and what lies in front of you, pales in comparison to what lies inside of you.", author:"Ralph Waldo Emerson"},
+    {quote:"Things work out best for those who make the best of how things work out.", author:"John Wooden"},
+    {quote:"If you are not willing to risk the usual you will have to settle for the ordinary.", author:"Jim Rohn"},
+    {quote:"Good things come to people who wait, but better things come to those who go out and get them.", author:"Anonymous"},
+    {quote:"Success is walking from failure to failure with no loss of enthusiasm.", author:"Winston Churchill"},
+    {quote:"If you do what you always did, you will get what you always got.", author:"Anonymous"},
+    {quote:"Whenever you see a successful person you only see the public glories, never the private sacrifices to reach them.", author:"Vaibhav Shah"},
+    {quote:"Try not to become a person of success, but rather try to become a person of value.", author:"Albert Einstein"},
+    {quote:"Great minds discuss ideas; average minds discuss events; small minds discuss people.", author:"Eleanor Roosevelt"},
+    {quote:"I have not failed. I've just found 10,000 ways that won't work.", author:"Thomas A. Edison"},
+    {quote:"If you're going through hell keep going.", author:"Winston Churchill"},
+    {quote:"The whole secret of a successful life is to find out what is one's destiny to do, and then do it.", author:"Henry Ford"},
+    {quote:"What seems to us as bitter trials are often blessings in disguise.", author:"Oscar Wilde"},
+    {quote:"If you can't explain it simply, you don't understand it well enough.", author:"Albert Einstein"},
+    {quote:"Life is not about finding yourself. Life is about creating yourself.", author:"Lolly Daskal"},
+    {quote:"There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.", author:"Ray Goforth"},
+    {quote:"The starting point of all achievement is desire.", author:"Napoleon Hill"},
+    {quote:"Success is the sum of small efforts, repeated day-in and day-out.", author:"Robert Collier"},
+    {quote:"All progress takes place outside the comfort zone.", author:"Michael John Bobak"},
+    {quote:"You may only succeed if you desire succeeding; you may only fail if you do not mind failing.", author:"Philippos"},
+    {quote:"Courage is resistance to fear, mastery of fear--not absence of fear.", author:"Mark Twain"},
+    {quote:"Only put off until tomorrow what you are willing to die having left undone.", author:"Pablo Picasso"},
+    {quote:"We become what we think about most of the time, and that's the strangest secret.", author:"Earl Nightingale"},
+    {quote:"The only place where success comes before work is in the dictionary.", author:"Vidal Sassoon"},
+    {quote:"Too many of us are not living our dreams because we are living our fears.", author:"Les Brown"},
+    {quote:"The road to success and the road to failure are almost exactly the same.", author:"Colin R. Davis"},
+    {quote:"The function of leadership is to produce more leaders, not more followers.", author:"Ralph Nader"},
+    {quote:"Success is liking yourself, liking what you do, and liking how you do it.", author:"Maya Angelou"},
+    {quote:"The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.", author:"Mark Caine"},
+    {quote:"Whenever you find yourself on the side of the majority, it is time to pause and reflect.", author:"Mark Twain"},
+    {quote:"The successful warrior is the average man, with laser-like focus.", author:"Bruce Lee"},
+    {quote:"Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.", author:"Dale Carnegie"},
+    {quote:"If you genuinely want something, don't wait for it--teach yourself to be impatient.", author:"Gurbaksh Chahal"},
+    {quote:"Don't let the fear of losing be greater than the excitement of winning.", author:"Robert Kiyosaki"},
+    {quote:"If you hear a voice within you say 'you cannot paint,' then by all means paint, and that voice will be silenced.", author:"Vincent Van Gogh"},
+    {quote:"Bran: Can a man be brave if he’s afraid? Ned Stark: That’s the only time he can be brave.", author:"Game Of Thrones"},
+    {quote:"A lion doesn’t concern himself with the opinions of a sheep.", author:"Tywin Lannister, Game Of Thrones"},
+    {quote:"A reader lives a thousand lives before he dies. The man who doesn’t read lives only one.", author:"Jojen Reed, Game Of Thrones"},
+    {quote:"Forgiveness is the final form of love.", author:"Reinhold Niebuhr"},
+    {quote:"To attain knowledge, add things everyday. To attain wisdom, remove things every day.", author:"Lao Tzu"},
+    {quote:"Wisdom is not a product of schooling but of the lifelong attempt to acquire it", author:" Albert Einstein"},
+    {quote:"Knowledge is power? No. Knowledge on its own is nothing, but the application of useful knowledge, now that is powerful.", author:"Rob Liano"},
+    {quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author:"Albert Einstein"},
+    {quote:"Creativity is intelligence having fun.", author:"Albert Einstein"},
+    {quote:"Man’s mind, once stretched by a new idea, never regains its original dimensions.", author:"Oliver Wendell Holmes"},
+    {quote:"Education is not preparation for life; education is life itself. ", author:"John Dewey"},
+    {quote:"Every student can learn, just not on the same day, or the same way.", author:"George Evans"},
+    {quote:"Children have to be educated, but they have also to be left to educate themselves.", author:"Ernest Dimnet"},
+    {quote:"You are always a student, never a master. You have to keep moving forward.", author:"Conrad Hall"},
+    {quote:"You can teach a student a lesson for a day; but if you can teach him to learn by creating curiosity, he will continue the learning process as long as he lives", author:"Clay P. Bedford"},
+    {quote:"Talent wins games, but teamwork and intelligence win championships.", author:"Michael Jordan"},
+    {quote:"It is literally true that you can succeed best and quickest by helping others to succeed.", author:"Napolean Hill"},
+    {quote:"None of us, including me, ever do great things. But we can all do small things, with great love, and together we can do something wonderful.", author:"Mother Teresa"},
+    {quote:"If I have seen further, it is by standing on the shoulders of giants", author:"Sir Isaac Newtoon"},
+    {quote:"Find a group of people who challenge and inspire you, spend a lot of time with them, and it will change your life.", author:"Amy Poehler"},
+    {quote:"Hard work beats talent if talent doesn't work hard.", author:"Tim Notke"},
+    {quote:"We think, mistakenly, that success is the result of the amount of time we put in at work, instead of the quality of time we put in.", author:"Ariana Huffington"},
+    {quote:"Someone once told me growth and comfort do not coexist. And I think it's a really good thing to remember.", author:"Ginni Rometty"},
+    {quote:"A leader takes people where they want to go. A great leader takes people where they don’t necessarily want to go, but ought to be.", author:"Rosalynn Carter"},
+    {quote:"Great leaders are almost always great simplifiers, who can cut through argument, debate and doubt to offer a solution everybody can understand.", author:"General Colin Powell"},
+    {quote:"A leader must inspire or his team will expire.", author:"Orrin Woodward"},
+    {quote:"No individual can win a game by himself.", author:"Pele"},
+    {quote:"Together, ordinary people can achieve extraordinary results.", author:"Becka Schoettle"},
+    {quote:"Coming together is a beginning. Keeping together is progress. Working together is success.", author:"Henry Ford"},
+    {quote:"He that cannot obey, cannot command.", author:"Benjamin Franklin"},
+    {quote:"We are what we repeatedly do. Excellence then is not an act, but a habit.", author:"Aristotle"},
+    {quote:"Except our own thoughts, there is nothing absolutely in our power.", author:"René Descartes"},
+    {quote:"The greatest win is walking away and choosing not to engage in drama and toxic energy at all.", author:"Lalah Delia"},
+    {quote:"Remember how far you’ve come, and you won’t have to rely on a destiny for your future. It will come on your own.", author:"Shannon A. Thompson"},
+    {quote:"If you wouldn't follow yourself, why should anyone else?", author:"John C. Maxwell"},
+    {quote:"Everything one has a right to do is not best to be done.", author:"Benjamin Franklin"},
+    {quote:"Teach yourself to be grateful no matter how little you have or receive. That's the first step to contentment. Self-discipline is the key.", author:"Verliza Gajeles"},
+    {quote:"Discipline not desire determines your destiny.", author:"Anonymous"},
+    {quote:"We all have dreams. But in order to make dreams come into reality, it takes an awful lot of determination, dedication, self-discipline, and effort.", author:"Jesse Owens"},
+    {quote:"The greatest religion to have ever walked our realm is humanity, know it, learn it, respect it and cultivate it", author:"Julius S. Oludare"},
+    {quote:"You must not lose faith in humanity. Humanity is an ocean; if a few drops of the ocean are dirty, the ocean does not become dirty.", author:"Mahatma Gandhi"},
+    {quote:"World belongs to humanity, not this leader, that leader or that king or prince or religious leader. World belongs to humanity.", author:"Dalai Lama"},
+    {quote:"The sole meaning of life is to serve humanity.", author:"Leo Tolstoy"},
+    {quote:"The greatness of humanity is not in being human, but in being humane.", author:"Mahatma Gandhi"}
+
+]
+
+let weekQuote = document.getElementById('weekQuote');
+let weekAuthor = document.getElementById('weekAuthor')
+
+
+
+
+document.getElementById('newQuote').addEventListener('click', displayQuote);
+
+function displayQuote() {
+    let script= quote[Math.floor(Math.random() *quote.length)];
+    console.log(script);
+    
+    weekQuote.innerHTML = script["quote"];
+    weekAuthor.innerHTML = script["author"];
+    
+}
+
+
+let soundBtn = document.querySelector('#sound');
+let copyBtn = document.querySelector('#copy');
+let twitterBtn = document.querySelector('#twitter');
+
+
+soundBtn.addEventListener('click', ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${weekQuote.innerHTML} by ${weekAuthor.innerHTML}`);
+    speechSynthesis.speak(utterance)
+});
+
+copyBtn.addEventListener('click', ()=>{
+   navigator.clipboard.writeText(weekQuote.innerHTML);
+   alert("Copied to clipboard");
+});
+
+twitterBtn.addEventListener('click', ()=>{
+     let tweetUrl = `https://twitter.com/intent/tweet/?url=${weekQuote.innerHTML}`;
+     window.open(tweetUrl, "_blank");
+ });
+
+
+
+
+ 
